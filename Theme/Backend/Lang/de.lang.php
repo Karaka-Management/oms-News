efd5467d3d6a2577ccc1c2d3e9b535470f784f7e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-04-05 19:31:56 +0200

    add/improve locale

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\News
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['News' => [
+    'Accounts/Groups' => 'Benutyer/Gruppen',
+    'Additional'      => 'Zusätzlich',
+    'Archive'         => 'Archiv',
+    'Author'          => 'Autor',
+    'Date'            => 'Datum',
+    'Draft'           => 'Entwurf',
+    'Featured'        => 'Featured',
+    'Groups'          => 'Gruppen',
+    'Headline'        => 'Headline',
+    'Headlines'       => 'Headlines',
+    'Link'            => 'Link',
+    'News'            => 'News',
+    'Plain'           => 'Plain',
+    'Preview'         => 'Vorschau',
+    'Publish'         => 'Veröffentlichen',
+    'Settings'        => 'Einstellung',
+    'Status'          => 'Status',
+    'Title'           => 'Titel',
+    'Type'            => 'Typ',
+    'TYPE0'           => 'Artikel',
+    'TYPE1'           => 'Link',
+    'TYPE2'           => 'Headline',
+    'Visible'         => 'Sichtbar',
+]];
