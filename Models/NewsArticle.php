cefb6595e9864b9c81678ee4523d92447ddd632f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-10 20:40:12 +0200

    fix typehint

--- Models/NewsArticle.php
+++ Models/NewsArticle.php
@@ -228,11 +228,11 @@ class NewsArticle implements \JsonSerializable, ArrayableInterface
     /**
      * Get date of creation
      *
-     * @return \DateTime
+     * @return \DateTimeImmutable
      *
      * @since 1.0.0
      */
-    public function getCreatedAt() : \DateTimeInterface
+    public function getCreatedAt() : \DateTimeImmutable
     {
         return $this->createdAt;
     }
