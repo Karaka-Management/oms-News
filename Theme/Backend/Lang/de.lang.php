ac329e3567b403b4989844f01fb4225066c475c2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

--- Theme/Backend/Lang/de.lang.php
+++ Theme/Backend/Lang/de.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\News
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,30 +12,31 @@
 declare(strict_types=1);
 
 return ['News' => [
-    'Accounts/Groups'    => 'Benutyer/Gruppen',
-    'Additional'         => 'Zusätzlich',
-    'AllowComments'      => 'Kommentare erlauben',
-    'Archive'            => 'Archiv',
-    'Author'             => 'Autor',
-    'Categories'         => 'Kategorien',
-    'Date'               => 'Datum',
-    'Draft'              => 'Entwurf',
-    'Featured'           => 'Featured',
-    'Groups'             => 'Gruppen',
-    'Headline'           => 'Headline',
-    'Headlines'          => 'Headlines',
-    'Langauge'           => 'Sprache',
-    'Link'               => 'Link',
-    'News'               => 'News',
-    'Plain'              => 'Plain',
-    'Preview'            => 'Vorschau',
-    'Publish'            => 'Veröffentlichen',
-    'Settings'           => 'Einstellung',
-    'Status'             => 'Status',
-    'Title'              => 'Titel',
-    'Type'               => 'Typ',
-    'TYPE0'              => 'Artikel',
-    'TYPE1'              => 'Link',
-    'TYPE2'              => 'Headline',
-    'Visible'            => 'Sichtbar',
+    'Accounts/Groups' => 'Benutyer/Gruppen',
+    'Additional' => 'Zusätzlich',
+    'AllowComments' => 'Kommentare erlauben',
+    'Archive' => 'Archiv',
+    'Author' => 'Autor',
+    'Categories' => 'Kategorien',
+    'Date' => 'Datum',
+    'Draft' => 'Entwurf',
+    'Featured' => 'Featured',
+    'Groups' => 'Gruppen',
+    'Headline' => 'Headline',
+    'Headlines' => 'Headlines',
+    'Langauge' => 'Sprache',
+    'Link' => 'Link',
+    'News' => 'News',
+    'Plain' => 'Plain',
+    'Preview' => 'Vorschau',
+    'Publish' => 'Veröffentlichen',
+    'Settings' => 'Einstellung',
+    'Status' => 'Status',
+    'Title' => 'Titel',
+    'Type' => 'Typ',
+    'TYPE0' => 'Artikel',
+    'TYPE1' => 'Link',
+    'TYPE2' => 'Headline',
+    'Visible' => 'Sichtbar',
+    'Language' => '',
 ]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\News
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -14,29 +13,30 @@ declare(strict_types=1);
 
 return ['News' => [
     'Accounts/Groups' => 'Accounts/Groups',
-    'Additional'      => 'Additional',
-    'AllowComments'   => 'Allow comments',
-    'Archive'         => 'Archive',
-    'Author'          => 'Author',
-    'Categories'      => 'Categories',
-    'Date'            => 'Date',
-    'Draft'           => 'Draft',
-    'Featured'        => 'Featured',
-    'Groups'          => 'Groups',
-    'Headline'        => 'Headline',
-    'Headlines'       => 'Headlines',
-    'Language'        => 'Language',
-    'Link'            => 'Link',
-    'News'            => 'News',
-    'Plain'           => 'Plain',
-    'Preview'         => 'Preview',
-    'Publish'         => 'Publish',
-    'Settings'        => 'Settings',
-    'Status'          => 'Status',
-    'Title'           => 'Title',
-    'Type'            => 'Type',
-    'TYPE0'           => 'Article',
-    'TYPE1'           => 'Link',
-    'TYPE2'           => 'Headline',
-    'Visible'         => 'Visible',
+    'Additional' => 'Additional',
+    'AllowComments' => 'Allow comments',
+    'Archive' => 'Archive',
+    'Author' => 'Author',
+    'Categories' => 'Categories',
+    'Date' => 'Date',
+    'Draft' => 'Draft',
+    'Featured' => 'Featured',
+    'Groups' => 'Groups',
+    'Headline' => 'Headline',
+    'Headlines' => 'Headlines',
+    'Langauge' => '',
+    'Link' => 'Link',
+    'News' => 'News',
+    'Plain' => 'Plain',
+    'Preview' => 'Preview',
+    'Publish' => 'Publish',
+    'Settings' => 'Settings',
+    'Status' => 'Status',
+    'Title' => 'Title',
+    'Type' => 'Type',
+    'TYPE0' => 'Article',
+    'TYPE1' => 'Link',
+    'TYPE2' => 'Headline',
+    'Visible' => 'Visible',
+    'Language' => 'Language',
 ]];
