42c9ba9954fb2822fc02dfe4430bb882c43aac71
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:06 +0200

    more tests and make classes final

---
+++ tests/Models/NullNewsArticle.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\News\tests\Models;
+
+use Modules\News\Models\NullNewsArticle;
+
+/**
+ * @internal
+ */
+final class Null extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\News\Models\NullNewsArticle
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\News\Models\NewsArticle', new NullNewsArticle());
+    }
+
+    /**
+     * @covers Modules\News\Models\NullNewsArticle
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullNewsArticle(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
---
+++ tests/Models/NullNewsSeenTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\News\tests\Models;
+
+use Modules\News\Models\NullNewsSeen;
+
+/**
+ * @internal
+ */
+final class NullNewsSeenTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\News\Models\NullNewsSeen
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\News\Models\NewsSeen', new NullNewsSeen());
+    }
+
+    /**
+     * @covers Modules\News\Models\NullNewsSeen
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullNewsSeen(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
