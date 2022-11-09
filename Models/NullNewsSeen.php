68d4678fe7d01cd66fc12506528b807343f49a6d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullNewsSeen.php
+++ Models/NullNewsSeen.php
@@ -36,4 +36,12 @@ final class NullNewsSeen extends NewsSeen
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
